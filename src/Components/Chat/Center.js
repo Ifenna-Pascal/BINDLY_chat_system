d37250f1255bd41941bb1452fa3f241af952ee11
Ifenna-Pascal <71331395+Ifenna-Pascal@users.noreply.github.com>
2021-11-26 01:44:19 +0100

    added profiile pics to icon

---
+++ src/Components/Chat/Center.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Center() {
+    return <div></div>;
+}
+
+export default Center;
--- src/Util/Icons.js
+++ src/Util/Icons.js
@@ -4,6 +4,7 @@ import GroupAddOutlinedIcon from '@material-ui/icons/GroupAddOutlined';
 import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
 import SettingsOutlinedIcon from '@material-ui/icons/SettingsOutlined';
 import NightsStayOutlined from '@material-ui/icons/NightsStayOutlined';
+import { Avatar } from '@material-ui/core';
 export const centerIcons = [
     {
         name: <PersonOutlinedIcon style={{ background: 'transparent' }} />,
@@ -24,6 +25,12 @@ export const bottomIcons = [
         name: <NightsStayOutlined style={{ background: 'transparent' }} />,
     },
     {
-        name: <GroupAddOutlinedIcon style={{ background: 'transparent' }} />,
+        name: (
+            <Avatar
+                style={{ width: '2.5rem', height: '2.5rem' }}
+                alt="Profile"
+                src="https://cdn.pixabay.com/photo/2018/01/06/09/25/hijab-3064633_960_720.jpg"
+            />
+        ),
     },
 ];
